fd4a3639ea83460fb2412c0bba8325db3b5bf232
arizona2014 <andy.lisac@gmail.com>
2016-04-26 20:52:37 +0200

    Small advancements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,33 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			questions:[
+				{
+					id: 1,
+					text: 'What is your name ?',
+					choices: [
+						{
+							id: 'a',
+							text: 'Michael'
+						},
+						{
+							id: 'b',
+							text: 'Brad'
+						},
+						{
+							id: 'c',
+							text: 'Steven'
+						}
+					],
+					correct: 'b'
+				},
+				
+			]
+		}
+	}
 	render(){
 		return(
 			<div>
