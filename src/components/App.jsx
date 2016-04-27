27dc1496c733b79c12c06d957e8a347ca172b0f8
Andy Lisac <andy.lisac@gmail.com>
2016-04-27 15:51:51 +0200

    Advancing on QuestionList and Question

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
 
 class App extends Component {
-	constructor(props){
+	constructor(props){				
 		super(props);
+		this.onChange = this.onChange.bind(this);
 		this.state = {
 			questions:[
 				{
@@ -25,14 +27,72 @@ class App extends Component {
 					],
 					correct: 'b'
 				},
-				
-			]
-		}
+				{
+					id: 2,
+					text: 'What is your mothers name ?',
+					choices: [
+						{
+							id: 'a',
+							text: 'Sara'
+						},
+						{
+							id: 'b',
+							text: 'Sue'
+						},
+						{
+							id: 'c',
+							text: 'Donna'
+						}
+					],
+					correct: 'c'
+				},				
+				{
+					id: 3,
+					text: 'What is your father name ?',
+					choices: [
+						{
+							id: 'a',
+							text: 'Bobby'
+						},
+						{
+							id: 'b',
+							text: 'Harry'
+						},
+						{
+							id: 'c',
+							text: 'Wayne'
+						}
+					],
+					correct: 'a'
+				},								
+				{
+					id: 4,
+					text: 'What is your friend name ?',
+					choices: [
+						{
+							id: 'a',
+							text: 'John'
+						},
+						{
+							id: 'b',
+							text: 'Paul'
+						},
+						{
+							id: 'c',
+							text: 'Dan'
+						}
+					],
+					correct: 'c'
+				}				
+			],
+			score: 0,
+			current: 1
+		}		
 	}
 	render(){
 		return(
 			<div>
-				APP
+				<QuestionList {...this.state} />
 			</div>
 		)
 	}
